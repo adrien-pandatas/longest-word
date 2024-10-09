59f5e807a6eaccd51a23189d86693b2a4770c8d1
adrien omont <adrien.omont@pandatas.com>
2024-10-09 12:33:36 +0000

    initialize project

---
+++ longest_word/game.py
@@ -0,0 +1,22 @@
+import string
+import random
+
+class Game:
+    def __init__(self) -> list:
+        """Attribute a random grid to size 9"""
+        self.grid = (random.choices(string.ascii_uppercase,k=9))
+
+    def is_valid(self, word: str) -> bool:
+        """Return True if and only if the word is valid, given the Game's grid"""
+        li = []
+        if word == '':
+            return False
+        else:
+            for letter in list(word):
+                if letter in self.grid:
+                    li.append(True)
+                else:
+                    li.append(False)
+        if False in li:
+            return False
+        return True
---
+++ tests/test_game.py
@@ -0,0 +1,43 @@
+# tests/test_game.py
+from longest_word.game import Game
+import string
+
+class TestGame:
+    def test_game_initialization(self):
+            # setup
+            game = Game()
+
+            # exercise
+            grid = game.grid
+
+            # verify
+            assert isinstance(grid, list)
+            assert len(grid) == 9
+            for letter in grid:
+                assert letter in string.ascii_uppercase
+
+            # teardown
+
+    def test_empty_word_is_invalid(self):
+        # setup
+        new_game = Game()
+        # verify
+        assert new_game.is_valid('') is False
+
+    def test_word_is_invalid(self):
+        # setup
+        new_game = Game()
+        word = "BLA"
+        new_grid = 'BHYFHJAOI'
+
+        new_game.grid = new_grid
+        assert new_game.is_valid(word) is False
+
+    def test_word_is_valid(self):
+        # setup
+        new_game = Game()
+        word = "JAY"
+        new_grid = 'BHYFHJAOI'
+
+        new_game.grid = new_grid
+        assert new_game.is_valid(word) is True
